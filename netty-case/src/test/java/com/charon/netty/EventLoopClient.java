f67d8f715582dfb7688d0343dd0a9d58a39c051e
charon <1178807730@qq.com>
2021-09-14 15:18:29 +0800

    eventloop的io任务(channel和eventloop绑定)

---
+++ netty-case/src/test/java/com/charon/netty/EventLoopClient.java
@@ -0,0 +1,45 @@
+package com.charon.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @program: netty
+ * @description
+ * @author: charon
+ * @create: 2021-09-14 15:00
+ **/
+@Slf4j
+public class EventLoopClient {
+    public static void main(String[] args) throws InterruptedException {
+        // 1. 启动类
+        Channel channel = new Bootstrap()
+                // 2. 添加 EventLoop
+                .group(new NioEventLoopGroup())
+                // 3. 选择客户端 channel 实现
+                .channel(NioSocketChannel.class)
+                // 4. 添加处理器
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    // 在连接建立后被调用
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                // 5. 连接到服务器
+                .connect(new InetSocketAddress("localhost", 8080))
+                // 阻塞方法:要使用 sync 方法等待 connect 建立连接完毕
+                .sync()
+                // 连接对象
+                .channel();
+        System.out.println(channel);
+        System.out.println("");
+    }
+}
---
+++ netty-case/src/test/java/com/charon/netty/EventLoopServer.java
@@ -0,0 +1,42 @@
+package com.charon.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+/**
+ * @program: netty
+ * @description
+ * @author: charon
+ * @create: 2021-09-14 14:53
+ **/
+@Slf4j
+public class EventLoopServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                            @Override
+                            // 此时数据类型为ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf byteBuf = (ByteBuf) msg;
+                                log.debug(byteBuf.toString(Charset.defaultCharset()));
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+}
