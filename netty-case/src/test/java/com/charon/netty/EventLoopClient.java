36059700845ffb4cfafb9eeb7da01301acfd90ab
charon <1178807730@qq.com>
2021-09-14 18:13:43 +0800

    channelFuture关闭问题

--- netty-case/src/test/java/com/charon/netty/EventLoopClient.java
+++ netty-case/src/test/java/com/charon/netty/EventLoopClient.java
@@ -8,9 +8,12 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 /**
  * @program: netty
@@ -21,13 +24,15 @@ import java.net.InetSocketAddress;
 @Slf4j
 public class EventLoopClient {
     public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
         // 2. 带有 Future，Promise 的类型都是和异步方法配套使用，用来处理结果
         ChannelFuture channelFuture = new Bootstrap()
-                .group(new NioEventLoopGroup())
+                .group(group)
                 .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                         nioSocketChannel.pipeline().addLast(new StringEncoder());
                     }
                 })
@@ -35,6 +40,7 @@ public class EventLoopClient {
                 // 异步非阻塞, main发起了调用，真正执行connect是nio线程
                 .connect(new InetSocketAddress("localhost", 8080));
 
+        /*---------------------------------处理结果--------------------------------------------*/
         // 2.1 使用 sync 方法同步处理结果
         /*channelFuture.sync(); // 阻塞住当前线程，直到nio线程连接建立完毕
         Channel channel = channelFuture.channel();
@@ -42,13 +48,44 @@ public class EventLoopClient {
         channel.writeAndFlush("hello, charon");*/
 
         // 2.2 使用 addListener(回调对象) 方法异步处理结果
-        channelFuture.addListener(new ChannelFutureListener() {
+        /*channelFuture.addListener(new ChannelFutureListener() {
             @Override
             // 在 nio 线程连接建立好之后，会调用 operationComplete
             public void operationComplete(ChannelFuture channelFuture) throws Exception {
                 Channel channel = channelFuture.channel();
                 log.debug("{}",channel);
                 channel.writeAndFlush("hello, charon");
+            }
+        });*/
+
+
+
+        /*---------------------------------关闭问题--------------------------------------------*/
+        Channel channel = channelFuture.sync().channel();
+        log.debug("{}",channel);
+        new Thread(()->{
+            Scanner scanner = new Scanner(System.in);
+            while (true){
+                String line = scanner.nextLine();
+                if("q".equalsIgnoreCase(line)){
+                    // close 异步操作
+                    channel.close();
+                    break;
+                }
+                channel.writeAndFlush(line);
+            }
+        },"input").start();
+
+        // 获取 CloseFuture 对象， 1) 同步处理关闭， 2) 异步处理关闭
+        ChannelFuture closeFuture = channel.closeFuture();
+        /*log.debug("waiting close...");
+        closeFuture.sync();
+        log.debug("处理关闭之后的操作");*/
+        closeFuture.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                log.debug("处理关闭之后的操作");
+                group.shutdownGracefully();
             }
         });
     }
