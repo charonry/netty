dbd731e0bbc7dbb5f62195ae2f8c05b31ad5b092
charon <1178807730@qq.com>
2021-09-14 11:16:10 +0800

    netty的客户端和服务端连接

---
+++ netty-case/src/test/java/com/charon/netty/HelloServer.java
@@ -0,0 +1,45 @@
+package com.charon.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @program: netty
+ * @description
+ * @author: charon
+ * @create: 2021-08-02 22:41
+ **/
+public class HelloServer {
+    public static void main(String[] args) {
+        // 1. 启动器，负责组装 netty 组件，启动服务器
+        new ServerBootstrap()
+                // 2. BossEventLoop, WorkerEventLoop(selector,thread), group 组
+                .group(new NioEventLoopGroup())
+                // 3. 选择服务器的ServerSocketChannel 实现
+                .channel(NioServerSocketChannel.class)
+                // 4. boss 负责处理连接 worker(child) 负责处理读写，决定了 worker(child) 能执行哪些操作（handler）
+                // 5. channel 代表和客户端进行数据读写的通道 Initializer 初始化，负责添加别的 handler
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        // 6. 添加具体 handler
+                        nioSocketChannel.pipeline().addLast(new StringDecoder());
+                        nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){// 自定义handler
+                            @Override
+                            // 读事件
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                })
+                // 7. 绑定监听端口
+                .bind(8080);
+    }
+}
