71b891d719c6f50c096dfe19b26d5749297bb354
charon <1178807730@qq.com>
2021-09-16 19:28:50 +0800

    EventLoop中的pipeline学习

---
+++ netty-case/src/test/java/com/charon/netty/Pipeline.java
@@ -0,0 +1,75 @@
+package com.charon.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @program: netty
+ * @description 管道流程的服务端
+ * @author: charon
+ * @create: 2021-09-16 17:14
+ **/
+@Slf4j
+public class Pipeline {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        // 1. 通过 channel 拿到 pipeline
+                        ChannelPipeline pipeline = nioSocketChannel.pipeline();
+                        // 2. 添加处理器 head -> 自定义handler-> tail
+                        pipeline.addLast("firstInboundHandler",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("1");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        pipeline.addLast("secondInboundHandler",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("2");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        pipeline.addLast("lastInboundHandler",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("3");
+                                super.channelRead(ctx, msg);
+                                nioSocketChannel.writeAndFlush(ctx.alloc().buffer().writeBytes("charon".getBytes()));
+                            }
+                        });
+                        pipeline.addLast("firstOutboundHandler",new ChannelOutboundHandlerAdapter(){
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("4");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("secondOutboundHandler",new ChannelOutboundHandlerAdapter(){
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("5");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("lastOutboundHandler",new ChannelOutboundHandlerAdapter(){
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("6");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+}
