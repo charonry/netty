70aa34a4374b55b6480dedbe357b10f5232c2be6
charon <1178807730@qq.com>
2021-11-06 19:52:21 +0800

    netty--backlog配置连接客户端数

---
+++ netty-case/src/test/java/com/charon/netty/optimize/TestBacklogServer.java
@@ -0,0 +1,30 @@
+package com.charon.netty.optimize;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @program: netty
+ * @description
+ * @author: charon
+ * @create: 2021-11-06 19:05
+ **/
+public class TestBacklogServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .option(ChannelOption.SO_BACKLOG, 2) // 全队列满了
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new LoggingHandler());
+                    }
+                }).bind(8080);
+    }
+}
